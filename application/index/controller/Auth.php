0b199f2a408549ede961f6f1eb4f1f32ecfd9cc1
HouserChang <changhaozhong66669@163.com>
2017-03-20 17:33:41 +0800

    'HouserChang做的完美注册!'

--- application/index/controller/Auth.php
+++ application/index/controller/Auth.php
@@ -28,36 +28,49 @@ class Auth extends Controller
         $this->assign('title','注册');
         return $this->fetch();
     }
+
+    /**
+     *   这是一个后台处理注册逻辑的方法，结合前台的ajax提交，当数据进来的时候先定义验证规则，在去获取ajax
+     * 提交的数据，并用定义好的规则去匹配，如果全部匹配的话就去数据库插入数据，需要注意的是把数据库没有的
+     * 字段过滤掉
+     * @param Request $request
+     * @param Users $user
+     * @return \think\response\Json
+     */
     public function doRegister(Request $request,Users $user)
     {
 //        dump($request->param());
-//        dump(input(post.));
-
+//        dump(input('post.ifpassword'));
+//        die;
         $ip = $_SERVER['REMOTE_ADDR'] === '::1'? '127.0.0.1':$_SERVER['REMOTE_ADDR'];
+
         $validate = new Validate([
             //定义验证规则
             'username' => 'require|max:20',
             'email' => 'email',
             'password'=>'require|length:3,50',
+            'ifpassword'=>'require|length:3,50',
         ]);
         $data = [
             'username' => input('post.username'),
             'email' => input('post.email'),
             'password'=>input('post.password'),
-            'create_ip' => ip2long($ip)
+            'create_ip' => ip2long($ip),
+            'ifpassword' => input('post.ifpassword')
         ];
         if (!$validate->check($data)) {
-
             return json(['status' => 0,'msg'=>$validate->getError()]);
+
         }else{
             $data['password'] = md5($data['password']);
-            $user->data($data);
-            $neuser = $user->save();
+            $userb = new Users($data);
 
-            if($neuser)
-            {
-                return json(['status' => 1]);
-            }
+//            $user->data($data);
+            $userb->allowField(true)->save();
+
+
+            return json(['status' => 1]);
+
 
         }
 //        Session::init([
@@ -72,6 +85,35 @@ class Auth extends Controller
 
 
     }
+
+    /**
+     * 处理邮箱和用户名的即时验证是否在数据库中存在
+     * @param Users $user
+     * @return \think\response\Json
+     */
+    public function doUser(Users $user)
+    {
+
+        if($user->where(['username' => input('post.username')] )->find())
+        {
+            return json(['status' => 1,'msg'=>'用户名重复了']);
+        }else{
+            return json(['status' => 0]);
+        }
+
+    }
+    public function doEmail(Users $user)
+    {
+
+        if($user->where(['email' => input('post.username')] )->find())
+        {
+            return json(['status' => 1,'msg'=>'邮箱重复了']);
+        }else{
+            return json(['status' => 0]);
+        }
+
+    }
+
     public function logOut()
     {
 
