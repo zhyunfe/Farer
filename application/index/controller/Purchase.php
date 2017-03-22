0075e5c3053ca2c4e170c7fbe4c2a6494b46fe88
HouserChang <changhaozhong66669@163.com>
2017-03-22 14:23:13 +0800

    '攻略模块第一次'

---
+++ application/index/controller/Purchase.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 11870
+ * Date: 2017/3/21
+ * Time: 23:26
+ */
---
+++ application/index/model/Purchase.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 11870
+ * Date: 2017/3/21
+ * Time: 23:26
+ */
+namespace app\indes\model;
+use think\Model;
+class Purchase extends Model
+{
+
+}
